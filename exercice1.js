8b282b872319fb3aee0a3bed855c52973cdf2a5d
ShyroVO <bosquet.laurie@outlook.com>
2020-12-14 09:36:37 +0100

    1.0

--- exercice1.js
+++ exercice1.js
@@ -2,15 +2,22 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let positionI = monTexte.indexOf('i');
+
+document.getElementById('position').innerHTML += positionI;
+
+let monTexteMaj = monTexte.toUpperCase();
+
+document.getElementById('chaineMaj').innerHTML = monTexteMaj;
 
 
-
-
-
-
-
+for (let i = 0; i < monTexte.length ; i++) {
+    if (monTexte[i].includes('i')){
+        alert("lettre a trouvé");
+    }
+}
